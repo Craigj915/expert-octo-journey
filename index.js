cfe1f8aaf3e7e79a7f36756cf0fab471a9bf7cf9
Utoki <85890871+NotUtoki@users.noreply.github.com>
2023-09-15 15:34:36 +0100

    Commiting all Updates

--- index.js
+++ index.js
@@ -3,29 +3,42 @@ let cName;
 let randomQuestion;
 let traits;
 let classes;
-
-// Initialize classes with all available classes
-fetch("class.json")
-  .then((response) => response.json())
-  .then((data) => {
-    classes = data.classes.slice(); // Copy the classes array   
-    console.log(classes);
-  });
+let className;
+let classesRemaining;
 
 //trait points
 let traitPoints = {
   male: 0,
   female: 0,
-  mobility: 0,
+  "hyper-mobile": 0,
   damage: 0,
   defence: 0,
   stealth: 0,
   support: 0,
-  range: 0,
+  "long-range": 0,
   melee: 0,
   tank: 0,
   assassin: 0,
   summon: 0,
+  shield: 0,
+  heal: 0,
+  spear: 0,
+  sword: 0,
+  axe: 0,
+  staff: 0,
+  agile: 0,
+  bow: 0,
+  gauntlet: 0,
+  bruiser: 0,
+  dodge: 0,
+  flight: 0,
+  magic: 0,
+  "huge-damage": 0,
+  ultimate: 0,
+  aoe: 0,
+  combo: 0,
+  "glass-cannon": 0,
+  "mid-range": 0,
 };
 
 const input = document.getElementById("charanameBox");
@@ -47,34 +60,62 @@ const validateCharaname = () => {
   }
 };
 
+function filterClasses(classes, traitPoints) {
+  return classes.filter((classData) => {
+    for (const trait in traitPoints) {
+      if (traitPoints[trait] > 0 && !classData[trait]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
 function nextQuestion() {
-  fetch("questions.json")
+  // Initialize classes with all available classes
+  fetch("class.json")
     .then((response) => response.json())
     .then((data) => {
-      const availableQuestions = data.filter(
-        (_, index) => !usedQuestionIndices.includes(index)
-      );
-      if (availableQuestions.length === 0) {
+      const classes = data.classes.slice();
+
+      // Filter classes based on traitPoints
+      classesRemaining = filterClasses(classes, traitPoints);
+
+      if (classesRemaining.length <= 3) {
         showSelection();
         return;
       }
 
-      randomQuestion =
-        availableQuestions[
-          Math.floor(Math.random() * availableQuestions.length)
-        ];
-      usedQuestionIndices.push(data.indexOf(randomQuestion)); // Keep track of used questions
-      document.getElementById("questionOutput").classList.remove("hidden");
-      document.getElementById("answers").classList.remove("hidden");
-      document.getElementById("questionOutput").innerHTML =
-        randomQuestion.question.replace("${cName}", cName);
-      console.log(availableQuestions);
-      //choices
-      for (let i = 1; i <= 3; i++) {
-        document.getElementById(`answer${i}`).innerHTML = randomQuestion.choice[
-          i
-        ].replace("${cName}", cName);
-      }
+      console.log(classesRemaining);
+
+      fetch("questions.json")
+        .then((response) => response.json())
+        .then((data) => {
+          const availableQuestions = data.filter(
+            (_, index) => !usedQuestionIndices.includes(index)
+          );
+          if (availableQuestions.length === 0) {
+            showSelection();
+            return;
+          }
+
+          randomQuestion =
+            availableQuestions[
+              Math.floor(Math.random() * availableQuestions.length)
+            ];
+          usedQuestionIndices.push(data.indexOf(randomQuestion)); // Keep track of used questions
+          document.getElementById("questionOutput").classList.remove("hidden");
+          document.getElementById("answers").classList.remove("hidden");
+          document.getElementById("questionOutput").innerHTML =
+            randomQuestion.question.replace("${cName}", cName);
+          console.log(availableQuestions);
+
+          //choices
+          for (let i = 1; i <= 3; i++) {
+            document.getElementById(`answer${i}`).innerHTML =
+              randomQuestion.choice[i].replace("${cName}", cName);
+          }
+        });
     });
 }
 
@@ -95,7 +136,47 @@ const addTraitPoint = (traits) => {
   console.log(traitPoints);
 };
 
+function getClassInfo(classData) {
+  for (let i = 0; i <= classData.length -1; i++) {
+    title = classData[i].info.title;
+    description = classData[i].info.description;
+    image = classData[i].info.image;
+
+    document.getElementById(`selectionTitle${i}`).innerHTML = title;
+    document.getElementById(`selectionDescription${i}`).innerHTML = description;
+    document.getElementById(`selectionImage${i}`).src = image;
+  }
+
+  if (classData.length == 2) {
+    document.getElementById("only2RemainingClasses").classList.add("hidden");
+    document
+      .getElementById("RemainingClassesGRID")
+      .classList.remove("grid-cols-3");
+    document
+      .getElementById("RemainingClassesGRID")
+      .classList.add("grid-cols-2");
+  } else if (classData.length < 2) {
+    document.getElementById("only1RemainingClasses").classList.add("hidden");
+    document.getElementById("only2RemainingClasses").classList.add("hidden");
+    document
+      .getElementById("RemainingClassesGRID")
+      .classList.remove("grid-cols-3");
+  }
+
+  // if (classData.length < 3) {
+  //   document.getElementById("only2RemainingClasses").classList.add("hidden");
+  //   document.getElementById("only2RemainingClassesGRID").classList.remove("grid-cols-3");
+  //   document.getElementById("only2RemainingClassesGRID").classList.add("grid-cols-2");
+  //   // only2RemainingClasses
+  //   // only2RemainingClassesGRID
+  // }
+
+  console.log(classData);
+  return classData.info;
+}
+
 function showSelection() {
+  getClassInfo(classesRemaining);
   document.getElementById("questionOutput").classList.add("hidden");
   document.getElementById("answers").classList.add("hidden");
   document.getElementById("characterSelect").classList.remove("hidden");
